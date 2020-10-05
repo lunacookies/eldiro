060c3978206b82ba6fd589299ef3973d87fd1425
Aramis Razzaghipour <aramisnoah@gmail.com>
2020-10-05 15:20:09 +1100

    Add parser for binding usages

---
+++ src/binding_usage.rs
@@ -0,0 +1,39 @@
+use crate::env::Env;
+use crate::utils;
+use crate::val::Val;
+
+#[derive(Debug, PartialEq)]
+pub struct BindingUsage {
+    name: String,
+}
+
+impl BindingUsage {
+    pub fn new(s: &str) -> Result<(&str, Self), String> {
+        let (s, name) = utils::extract_ident(s)?;
+
+        Ok((
+            s,
+            Self {
+                name: name.to_string(),
+            },
+        ))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_binding_usage() {
+        assert_eq!(
+            BindingUsage::new("abc"),
+            Ok((
+                "",
+                BindingUsage {
+                    name: "abc".to_string(),
+                },
+            )),
+        )
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 pub mod binding_def;
+pub mod binding_usage;
 pub mod expr;
 pub mod val;
 
