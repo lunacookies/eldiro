2f00f6e3505682633552c6417cbaa6a2601f13ae
Aramis Razzaghipour <aramisnoah@gmail.com>
2020-10-05 18:16:56 +1100

    Add quotes around binding name in nonexistent binding error message

--- src/env.rs
+++ src/env.rs
@@ -15,6 +15,6 @@ impl Env {
         self.bindings
             .get(name)
             .cloned()
-            .ok_or_else(|| format!("binding with name {} does not exist", name))
+            .ok_or_else(|| format!("binding with name ‘{}’ does not exist", name))
     }
 }
