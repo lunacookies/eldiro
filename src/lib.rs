05bacf447bedef560e5caceb1bc718e762117a5f
Aramis Razzaghipour <aramisnoah@gmail.com>
2020-10-07 12:16:09 +1100

    Add simple public interface for parsing

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,15 @@ pub mod val;
 
 mod env;
 mod utils;
+
+pub struct Parse(stmt::Stmt);
+
+pub fn parse(s: &str) -> Result<Parse, String> {
+    let (s, stmt) = stmt::Stmt::new(s)?;
+
+    if s.is_empty() {
+        Ok(Parse(stmt))
+    } else {
+        Err("input was not consumed fully by parser".to_string())
+    }
+}
