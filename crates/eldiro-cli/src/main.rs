a6a9767fc45729f2e53cbf44899ceb48e9c4e4be
Aramis Razzaghipour <aramisnoah@gmail.com>
2020-10-07 20:17:23 +1100

    Use Display impl of Val to display evaluated value in REPL

--- crates/eldiro-cli/src/main.rs
+++ crates/eldiro-cli/src/main.rs
@@ -15,27 +15,25 @@ fn main() -> io::Result<()> {
         stdin.read_line(&mut input)?;
 
         match run(input.trim(), &mut env) {
-            Ok(()) => {}
-            Err(msg) => {
-                writeln!(stderr, "{}", msg)?;
-                stderr.flush()?;
-            }
+            Ok(Some(val)) => writeln!(stdout, "{}", val)?,
+            Ok(None) => {}
+            Err(msg) => writeln!(stderr, "{}", msg)?,
         }
 
         input.clear();
     }
 }
 
-fn run(input: &str, env: &mut eldiro::Env) -> Result<(), String> {
+fn run(input: &str, env: &mut eldiro::Env) -> Result<Option<eldiro::Val>, String> {
     let parse = eldiro::parse(input).map_err(|msg| format!("Parse error: {}", msg))?;
 
     let evaluated = parse
         .eval(env)
         .map_err(|msg| format!("Evaluation error: {}", msg))?;
 
-    if evaluated != eldiro::Val::Unit {
-        dbg!(evaluated);
+    if evaluated == eldiro::Val::Unit {
+        Ok(None)
+    } else {
+        Ok(Some(evaluated))
     }
-
-    Ok(())
 }
