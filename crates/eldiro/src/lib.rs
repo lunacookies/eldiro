3000a534a6ed1f880fe91366b3e71157a6600879
Aramis Razzaghipour <aramisnoah@gmail.com>
2020-11-09 19:36:04 +1100

    Wipe everything

--- crates/eldiro/src/lib.rs
+++ crates/eldiro/src/lib.rs
@@ -1,29 +1,7 @@
-mod binding_def;
-mod env;
-mod expr;
-mod func_def;
-mod stmt;
-mod utils;
-mod val;
-
-pub use env::Env;
-pub use val::Val;
-
-#[derive(Debug)]
-pub struct Parse(stmt::Stmt);
-
-impl Parse {
-    pub fn eval(&self, env: &mut Env) -> Result<Val, String> {
-        self.0.eval(env)
-    }
-}
-
-pub fn parse(s: &str) -> Result<Parse, String> {
-    let (s, stmt) = stmt::Stmt::new(s)?;
-
-    if s.is_empty() {
-        Ok(Parse(stmt))
-    } else {
-        Err("input was not consumed fully by parser".to_string())
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
     }
 }
