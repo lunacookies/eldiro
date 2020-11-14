5d15ff63472ce6ba9089b3e508134a7c3aa7f2d3
Aramis Razzaghipour <aramisnoah@gmail.com>
2020-11-14 13:09:27 +1100

    Add initial implementation of Eldiro CLI

--- crates/eldiro-cli/src/main.rs
+++ crates/eldiro-cli/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use eldiro::parser::Parser;
+use std::io::{self, Write};
+
+fn main() -> io::Result<()> {
+    let stdin = io::stdin();
+    let mut stdout = io::stdout();
+
+    let mut input = String::new();
+
+    loop {
+        write!(stdout, "→ ")?;
+        stdout.flush()?;
+
+        stdin.read_line(&mut input)?;
+
+        let parse = Parser::new(&input).parse();
+        println!("{}", parse.debug_tree());
+
+        input.clear();
+    }
 }
