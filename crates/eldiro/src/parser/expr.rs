eb2c74fcce3dc32cbf1786f75a1be4d3d77bbae8
Aramis Razzaghipour <aramisnoah@gmail.com>
2020-11-14 23:27:54 +1100

    Implement parsing of binary operations with operator precedence

--- crates/eldiro/src/parser/expr.rs
+++ crates/eldiro/src/parser/expr.rs
@@ -2,9 +2,54 @@ use super::Parser;
 use crate::lexer::SyntaxKind;
 
 pub(super) fn expr(p: &mut Parser) {
+    expr_binding_power(p, 0);
+}
+
+fn expr_binding_power(p: &mut Parser, minimum_binding_power: u8) {
+    let checkpoint = p.checkpoint();
+
     match p.peek() {
         Some(SyntaxKind::Number) | Some(SyntaxKind::Ident) => p.bump(),
         _ => {}
+    }
+
+    loop {
+        let op = match p.peek() {
+            Some(SyntaxKind::Plus) => Op::Add,
+            Some(SyntaxKind::Minus) => Op::Sub,
+            Some(SyntaxKind::Star) => Op::Mul,
+            Some(SyntaxKind::Slash) => Op::Div,
+            _ => return, // we’ll handle errors later.
+        };
+
+        let (left_binding_power, right_binding_power) = op.binding_power();
+
+        if left_binding_power < minimum_binding_power {
+            return;
+        }
+
+        // Eat the operator’s token.
+        p.bump();
+
+        p.start_node_at(checkpoint, SyntaxKind::BinOp);
+        expr_binding_power(p, right_binding_power);
+        p.finish_node();
+    }
+}
+
+enum Op {
+    Add,
+    Sub,
+    Mul,
+    Div,
+}
+
+impl Op {
+    fn binding_power(&self) -> (u8, u8) {
+        match self {
+            Self::Add | Self::Sub => (1, 2),
+            Self::Mul | Self::Div => (3, 4),
+        }
     }
 }
 
@@ -30,6 +75,57 @@ Root@0..3
             expect![[r#"
 Root@0..7
   Ident@0..7 "counter""#]],
+        );
+    }
+
+    #[test]
+    fn parse_simple_binary_operation() {
+        check(
+            "1+2",
+            expect![[r#"
+Root@0..3
+  BinOp@0..3
+    Number@0..1 "1"
+    Plus@1..2 "+"
+    Number@2..3 "2""#]],
+        );
+    }
+
+    #[test]
+    fn parse_left_associative_binary_operation() {
+        check(
+            "1+2+3+4",
+            expect![[r#"
+Root@0..7
+  BinOp@0..7
+    BinOp@0..5
+      BinOp@0..3
+        Number@0..1 "1"
+        Plus@1..2 "+"
+        Number@2..3 "2"
+      Plus@3..4 "+"
+      Number@4..5 "3"
+    Plus@5..6 "+"
+    Number@6..7 "4""#]],
+        );
+    }
+
+    #[test]
+    fn parse_binary_operation_with_mixed_binding_power() {
+        check(
+            "1+2*3-4",
+            expect![[r#"
+Root@0..7
+  BinOp@0..7
+    BinOp@0..5
+      Number@0..1 "1"
+      Plus@1..2 "+"
+      BinOp@2..5
+        Number@2..3 "2"
+        Star@3..4 "*"
+        Number@4..5 "3"
+    Minus@5..6 "-"
+    Number@6..7 "4""#]],
         );
     }
 }
