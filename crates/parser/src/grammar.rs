60c9a3d8c0c45c7ec7a4e1c01f9b474ccc6c76fb
Aramis Razzaghipour <aramisnoah@gmail.com>
2020-12-18 15:23:42 +1100

    Parse multiple statements

--- crates/parser/src/grammar.rs
+++ crates/parser/src/grammar.rs
@@ -7,7 +7,37 @@ use syntax::SyntaxKind;
 
 pub(crate) fn root(p: &mut Parser) -> CompletedMarker {
     let m = p.start();
-    stmt::stmt(p);
+
+    while !p.at_end() {
+        stmt::stmt(p);
+    }
 
     m.complete(p, SyntaxKind::Root)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::check;
+    use expect_test::expect;
+
+    #[test]
+    fn parse_multiple_statements() {
+        check(
+            "let a = 1\na",
+            expect![[r#"
+Root@0..11
+  VariableDef@0..10
+    LetKw@0..3 "let"
+    Whitespace@3..4 " "
+    Ident@4..5 "a"
+    Whitespace@5..6 " "
+    Equals@6..7 "="
+    Whitespace@7..8 " "
+    Literal@8..10
+      Number@8..9 "1"
+      Whitespace@9..10 "\n"
+  VariableRef@10..11
+    Ident@10..11 "a""#]],
+        );
+    }
 }
