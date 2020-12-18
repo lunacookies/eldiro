7ce100e48d2323877004b0873fb03548b0c9b16d
Aramis Razzaghipour <aramisnoah@gmail.com>
2020-12-18 15:23:58 +1100

    Add test for ‘let’ token error recovery

--- crates/parser/src/grammar/stmt.rs
+++ crates/parser/src/grammar/stmt.rs
@@ -42,4 +42,29 @@ Root@0..13
       Ident@10..13 "bar""#]],
         );
     }
+
+    #[test]
+    fn recover_on_let_token() {
+        check(
+            "let a =\nlet b = a",
+            expect![[r#"
+Root@0..17
+  VariableDef@0..8
+    LetKw@0..3 "let"
+    Whitespace@3..4 " "
+    Ident@4..5 "a"
+    Whitespace@5..6 " "
+    Equals@6..7 "="
+    Whitespace@7..8 "\n"
+  VariableDef@8..17
+    LetKw@8..11 "let"
+    Whitespace@11..12 " "
+    Ident@12..13 "b"
+    Whitespace@13..14 " "
+    Equals@14..15 "="
+    Whitespace@15..16 " "
+    VariableRef@16..17
+      Ident@16..17 "a""#]],
+        );
+    }
 }
