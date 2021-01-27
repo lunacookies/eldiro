b1aa98ec646c989442fc1e65185b4ca32665afe5
Aramis Razzaghipour <aramisnoah@gmail.com>
2021-01-27 15:17:16 +1100

    Fuzz AST validation

--- fuzz/fuzz_targets/parse_lower.rs
+++ fuzz/fuzz_targets/parse_lower.rs
@@ -5,7 +5,9 @@ use libfuzzer_sys::fuzz_target;
 fuzz_target!(|data: &[u8]| {
     if let Ok(s) = std::str::from_utf8(data) {
         let parse = parser::parse(s);
-        let root = ast::Root::cast(parse.syntax()).unwrap();
+        let syntax = parse.syntax();
+        let _validation_errors = ast::validation::validate(&syntax);
+        let root = ast::Root::cast(syntax).unwrap();
         let (_database, _stmts) = hir::lower(root);
     }
 });
